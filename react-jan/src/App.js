804dd1d85f795d35b0e0b596d44fe06dd6abf80f
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-02-11 18:02:23 +0530

    okie

---
+++ react-jan/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Product from './Product/Product'
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>App Component</h1>
+                <hr />
+                <Product />
+            </div>
+        )
+    }
+}
+
+export default App
